571328367770dc7b157f8bf6239ecd233738f4b5
Kevin <kevinoc554@gmail.com>
2021-07-20 12:57:48 +0000

    feat: create User class, create method on registration form class to pass data along

--- app.py
+++ app.py
@@ -16,6 +16,29 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Classes
+class User():
+    """
+    A class that represents a User,
+    and allows for the relevant db operations
+    """
+
+    def __init__(self, fname, lname, username, email, password, _id=None):
+        """
+        Initialize an instance of the User class
+        """
+        self.fname = fname
+        self.lname = lname
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+# Routes
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -28,10 +51,20 @@ def all_games():
     return render_template("all-games.html", games=games, title='All Games')
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        # register = {
+        #     "fname": request.form.get("fname"),
+        #     "lname": request.form.get("lname"),
+        #     "username": request.form.get("username"),
+        #     "email": request.form.get("email"),
+        #     "password": request.form.get("password")
+        # }
+        register = form.make_dict()
+        new_user = User(**register)
+        print(repr(new_user))
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
--- forms.py
+++ forms.py
@@ -16,3 +16,17 @@ class RegistrationForm(FlaskForm):
     confirm = PasswordField('Confirm Password', validators=[
         InputRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up!')
+
+    def make_dict(self):
+        """
+        Takes response to form and converts to dict,
+        ready to pass to User class
+        """
+        info = {
+            "fname": self.fname.data,
+            "lname": self.lname.data,
+            "username": self.username.data,
+            "email": self.email.data,
+            "password": self.password.data
+        }
+        return info
