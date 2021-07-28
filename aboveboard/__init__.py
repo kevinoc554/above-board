0dd5379dfd2ee78ec73611d8d93cebae287a9dcd
Kevin <kevinoc554@gmail.com>
2021-07-28 13:58:15 +0000

    feat: set the login message category, and add styles

--- aboveboard/__init__.py
+++ aboveboard/__init__.py
@@ -16,6 +16,7 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 from aboveboard import routes
