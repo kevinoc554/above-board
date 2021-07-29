b4121a122e0d37484c7bfa906b05cb2f70bef884
Kevin <kevinoc554@gmail.com>
2021-07-29 15:26:46 +0000

    testing: add unit tests for GET requests to all-games, login and register routes

--- test.py
+++ test.py
@@ -2,11 +2,49 @@ from aboveboard import app
 from unittest import TestCase, main
 
 
-class FirstTestCase(TestCase):
+class TestGetRoutes(TestCase):
+    """
+    Unit tests for basic GET routes.
+    """
 
-    def test_index(self):
+    def test_home(self):
+        """
+        Test GET requests to Home route.
+        Expected Results:
+        Response - 200
+        """
         client = app.test_client(self)
         response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_games(self):
+        """
+        Test GET requests to All Games route.
+        Expected Results:
+        Response - 200
+        """
+        client = app.test_client(self)
+        response = client.get('/all-games')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        """
+        Test GET requests to Login route.
+        Expected Results:
+        Response - 200
+        """
+        client = app.test_client(self)
+        response = client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        """
+        Test GET requests to Register route.
+        Expected Results:
+        Response - 200
+        """
+        client = app.test_client(self)
+        response = client.get('/register')
         self.assertEqual(response.status_code, 200)
 
 
