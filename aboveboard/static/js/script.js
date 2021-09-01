69cc6d77f5ca73c13813b294857a10aa2e2f837d
Kevin <kevinoc554@gmail.com>
2021-09-01 12:58:45 +0000

    feat: add JS to disable the first option in each of the dynamically created select elements

--- aboveboard/static/js/script.js
+++ aboveboard/static/js/script.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-  });
+$(document).ready(function () {
+  disableOption()
+  initMaterialize();
+});
+
+// Initializes all relevant JS for MaterializeCSS components
+function initMaterialize() {
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+}
+
+// Applies the 'disabled' property to the first option 
+// in each of the dynamically created select elements
+function disableOption() {
+  $('#genre').children().first().prop('disabled', true);
+  $('#mechanics').children().first().prop('disabled', true);
+}
