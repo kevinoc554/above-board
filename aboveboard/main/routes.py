b8607fd0021a6bcd25d545e4fb3c6567fdd93e55
Kevin <kevinoc554@gmail.com>
2021-09-17 13:10:14 +0000

    feat: update home route to get most recent 3 games in db and display on home page

--- aboveboard/main/routes.py
+++ aboveboard/main/routes.py
@@ -1,4 +1,5 @@
-from flask import (render_template, Blueprint)
+from flask import render_template, Blueprint
+from aboveboard import mongo
 
 main = Blueprint('main', __name__)
 
@@ -6,4 +7,10 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template("home.html")
+    # sort through db in reverse order and get the 3 most recent additions
+    games = mongo.db.games.find().hint([('$natural', -1)]).limit(3)
+    games = list(games)
+    # set ratings as a rounded average of the list of ints
+    for game in games:
+        game['rating'] = round(sum(game['rating'])/len(game['rating']))
+    return render_template("home.html", games=games)
